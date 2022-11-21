02896a07e9bf3afbecdc963d17421b6ff85a40d8
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-11-21 17:36:24 -0300

    try to fix Funcrion users

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-export function App(){
-  return(
-    <h1>hello</h1>
-  )
+import { useState } from "react";
+import { Users } from "./Components/Users";
+import { FormEvent } from "react";
+type UsersProps = {
+  id: number;
+  name: string;
+  email: string;
+};
+export function App() {
+  const [filter, setFilter] = useState("");
+  const [users, setUsers] = useState<UsersProps[]>([]);
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:3333//${filter}`);
+    const data = await response.json();
+    setUsers(data);
+  }
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+        &nbsp;
+        <button type="submit">Procurar</button>
+      </form>
+      <Users users={users} />
+    </div>
+  );
 }
